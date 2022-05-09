cac93080af984fe04f00a5aa0a0f5daff7874aa9
Imtiyaz <76092300+IntiTalent@users.noreply.github.com>
2022-05-09 11:35:48 +0530

    making navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+      <Router>
+        <Header />
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <>
+            <header className='text-gray-400 bg-gray-900 body-font'>
+                <div className='container mx-auto flex flex-wrap items-center flex-col md:flex-row p-4'>
+                    <a href="" className='hover:text-gray-200'>
+                        <span className="ml-3 text-xl">News Mate</span>
+                    </a>
+                    <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center'>
+                        <a className="nav-item mr-5 hover:text-gray-200 "><Link className="nav-link active" aria-current="page" to="/">Home</Link></a>
+                        <a className="nav-item mr-5 hover:text-gray-200 "><Link className="nav-link active" aria-current="page" to="/business">Business</Link></a>
+                        <a className="nav-item mr-5 hover:text-gray-200 "><Link className="nav-link active" aria-current="page" to="/entertainment">Entertainment</Link></a>
+                        <a className="nav-item mr-5 hover:text-gray-200 "><Link className="nav-link active" aria-current="page" to="/health">Health</Link></a>
+                        <a className="nav-item mr-5 hover:text-gray-200 "><Link className="nav-link active" aria-current="page" to="/science">Science</Link></a>
+                        <a className="nav-item mr-5 hover:text-gray-200 "><Link className="nav-link active" aria-current="page" to="/sports">Sports</Link></a>
+                        <a className="nav-item mr-5 hover:text-gray-200 "><Link className="nav-link active" aria-current="page" to="/technology">Technology</Link></a>
+                    </nav>
+                    {/* <button className='bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded'>
+                        Button
+                    </button> */}
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
